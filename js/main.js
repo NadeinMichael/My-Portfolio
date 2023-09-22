c175f22d18d54e70fe8371eb476fba2e82b62ccf
Nadein Michael <nadein.michael@gmail.com>
2023-09-22 16:44:02 +0700

    feat: add js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
-const btnDarkMode = document.querySelector(".dark-mode-btn");
+const btnDarkMode = document.querySelector('.dark-mode-btn');
 
-btnDarkMode.onclick
+const addDarkTheme = () => {
+  btnDarkMode.classList.add('dark-mode-btn--active');
+
+  document.body.classList.add('dark');
+};
+
+const removeDarkTheme = () => {
+  btnDarkMode.classList.remove('dark-mode-btn--active');
+
+  document.body.classList.remove('dark');
+};
+
+// Checking dark theme in system settings
+if (
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+) {
+  addDarkTheme();
+}
+
+// Checking dark theme in local storage
+if (localStorage.getItem('darkMode') === 'dark') {
+  addDarkTheme();
+} else {
+  removeDarkTheme();
+}
+
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', (event) => {
+    const newColorTheme = event.matches ? 'dark' : 'light';
+
+    if (newColorTheme === 'dark') {
+      addDarkTheme();
+    } else {
+      removeDarkTheme();
+    }
+  });
+
+// toggler dark theme
+btnDarkMode.onclick = () => {
+  btnDarkMode.classList.toggle('dark-mode-btn--active');
+
+  const isDark = document.body.classList.toggle('dark');
+
+  if (isDark) {
+    localStorage.setItem('darkMode', 'dark');
+  } else {
+    localStorage.setItem('darkMode', 'light');
+  }
+};
